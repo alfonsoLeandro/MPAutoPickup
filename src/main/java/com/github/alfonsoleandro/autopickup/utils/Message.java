257435725be8a2ebd0f3c36a1b5b1b6144167a8e
Leandro Alfonso <leamanya24.6@gmail.com>
2021-12-16 13:08:49 -0300

    Implemented Messages enum

--- src/main/java/com/github/alfonsoleandro/autopickup/utils/Message.java
+++ src/main/java/com/github/alfonsoleandro/autopickup/utils/Message.java
@@ -3,7 +3,25 @@ package com.github.alfonsoleandro.autopickup.utils;
 import com.github.alfonsoleandro.mputils.misc.MessageEnum;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Locale;
+
 public enum Message implements MessageEnum {
+    AP_BLOCKS_ENABLED("config.messages.autoPickup.blocks.enabled","&fBlock drops AutoPickup &fhas been &aenabled &ffor you"),
+    AP_BLOCKS_DISABLED("config.messages.autoPickup.blocks.disabled","&fBlock drops AutoPickup &fhas been &cdisabled &ffor you"),
+    AP_MOB_ENABLED("config.messages.autoPickup.mob.enabled","&fMob drops AutoPickup &fhas been &aenabled &ffor you"),
+    AP_MOB_DISABLED("config.messages.autoPickup.mob.disabled","&fMob drops AutoPickup &fhas been &cdisabled &ffor you"),
+    AP_EXP_ENABLED("config.messages.autoPickup.exp.enabled","&fExperience AutoPickup &fhas been &aenabled &ffor you"),
+    AP_EXP_DISABLED("config.messages.autoPickup.exp.disabled","&fExperience AutoPickup &fhas been &cdisabled &ffor you"),
+    AS_BLOCKS_ENABLED("config.messages.autoSmelt.blocks.enabled","&fBlock drops AutoSmelt &fhas been &aenabled &ffor you"),
+    AS_BLOCKS_DISABLED("config.messages.autoSmelt.blocks.disabled","&fBlock drops AutoSmelt &fhas been &cdisabled &ffor you"),
+    AS_MOB_ENABLED("config.messages.autoSmelt.mob.enabled","&fMob drops AutoSmelt &fhas been &aenabled &ffor you"),
+    AS_MOB_DISABLED("config.messages.autoSmelt.mob.disabled","&fMob drops AutoSmelt &fhas been &cdisabled &ffor you"),
+
+    CANNOT_CONSOLE("config.messages.cannot send from console", "&cThat command can only be sent by a player"),
+    FULL_INV("&cYour inventory is full. &fSome items may be dropped to the ground"),
+    NO_PERMISSION("&cNo permission"),
+    PLACEHOLDER_ENABLED("config.messages.placeholder status.enabled", "&aEnabled"),
+    PLACEHOLDER_DISABLED("config.messages.placeholder status.disabled", "&cDisabled"),
 
 
     ;
@@ -16,15 +34,22 @@ public enum Message implements MessageEnum {
         this.dflt = dflt;
     }
 
+    Message(String dflt){
+        this(null, dflt);
+    }
+
     @NotNull
     @Override
     public String getPath() {
-        return null;
+        return this.path == null ?
+                "config.messages."+this.toString().toLowerCase(Locale.ROOT).replace("_"," ")
+                :
+                this.path;
     }
 
     @NotNull
     @Override
     public String getDefault() {
-        return null;
+        return this.dflt;
     }
 }
