58b36f9d7f7c4a637e287f70edd993eb032123d4
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2022-02-26 03:16:08 -0300

    Minor changes on what @Fabri did. Found a bug related to the new permissions. Added the new permissions to the plugin.yml file.

--- src/main/java/com/github/alfonsoleandro/autopickup/commands/MainCommand.java
+++ src/main/java/com/github/alfonsoleandro/autopickup/commands/MainCommand.java
@@ -18,16 +18,17 @@ public class MainCommand implements CommandExecutor {
     private final AutoPickup plugin;
     private final MessageSender<Message> messageSender;
     private final Settings settings;
+    private final ChangeSettings changeSettings;
 
     public MainCommand(AutoPickup plugin) {
         this.plugin = plugin;
         this.messageSender = plugin.getMessageSender();
         this.settings = plugin.getSettings();
+        this.changeSettings = new ChangeSettings(plugin);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
-        ChangeSettings changeSettings = new ChangeSettings(plugin);
 
         if (args.length == 0 || args[0].equalsIgnoreCase("help")) {
             this.messageSender.send(sender, "&c&lREQUIRED&c: []&8, &eOPTIONAL: {}");
@@ -75,7 +76,7 @@ public class MainCommand implements CommandExecutor {
 
             //if setting doesn't exist
             String settingToChange = args[1];
-            if (!changeSettings.settingExists(settingToChange)) {
+            if (this.changeSettings.settingNotExists(settingToChange)) {
                 this.messageSender.send(sender, "&cUnknown setting, check the Spigot's page for help.");
                 return true;
             }
@@ -87,12 +88,12 @@ public class MainCommand implements CommandExecutor {
                     this.messageSender.send(sender, "&cSorry, but the player you tried to change their settings doesn't seem to be online.");
                     return true;
                 }
-                changeSettings.toggleSetting(settingToChange, playerToUse, true);
+                this.changeSettings.toggleSetting(settingToChange, playerToUse, true);
                 return true;
             }
 
             Player player = (Player) sender;
-            changeSettings.toggleSetting(settingToChange, player, false);
+            this.changeSettings.toggleSetting(settingToChange, player, false);
             return true;
         }
         if(args[0].equalsIgnoreCase("enable")){
@@ -101,13 +102,13 @@ public class MainCommand implements CommandExecutor {
                 return true;
             }
             if(args.length == 1){
-                messageSender.send(sender, "&cYou need to specify a setting to turn on");
+                this.messageSender.send(sender, "&cYou need to specify a setting to turn on");
                 return true;
             }
 
             //if setting doesn't exist
             String settingToChange = args[1];
-            if (!changeSettings.settingExists(settingToChange)) {
+            if (this.changeSettings.settingNotExists(settingToChange)) {
                 this.messageSender.send(sender, "&cUnknown setting, check the Spigot's page for help.");
                 return true;
             }
@@ -118,12 +119,12 @@ public class MainCommand implements CommandExecutor {
                     this.messageSender.send(sender, "&cSorry, but the player you tried to change their settings doesn't seem to be online.");
                     return true;
                 }
-                changeSettings.enableSetting(settingToChange, playerToUse, true);
+                this.changeSettings.enableSetting(settingToChange, playerToUse, true);
                 return true;
             }
 
             Player player = (Player) sender;
-            changeSettings.enableSetting(settingToChange, player, false);
+            this.changeSettings.enableSetting(settingToChange, player, false);
             return true;
         }
 
@@ -133,13 +134,13 @@ public class MainCommand implements CommandExecutor {
                 return true;
             }
             if(args.length == 1){
-                messageSender.send(sender, "&cYou need to specify a setting to turn off");
+                this.messageSender.send(sender, "&cYou need to specify a setting to turn off");
                 return true;
             }
 
             //if setting doesn't exist
             String settingToChange = args[1];
-            if (!changeSettings.settingExists(settingToChange)) {
+            if (this.changeSettings.settingNotExists(settingToChange)) {
                 this.messageSender.send(sender, "&cUnknown setting, check the Spigot's page for help.");
                 return true;
             }
@@ -150,12 +151,12 @@ public class MainCommand implements CommandExecutor {
                     this.messageSender.send(sender, "&cSorry, but the player you tried to change their settings doesn't seem to be online.");
                     return true;
                 }
-                changeSettings.disableSetting(settingToChange, playerToUse, true);
+                this.changeSettings.disableSetting(settingToChange, playerToUse, true);
                 return true;
             }
 
             Player player = (Player) sender;
-            changeSettings.disableSetting(settingToChange, player, false);
+            this.changeSettings.disableSetting(settingToChange, player, false);
             return true;
         }
 
--- src/main/java/com/github/alfonsoleandro/autopickup/utils/ChangeSettings.java
+++ src/main/java/com/github/alfonsoleandro/autopickup/utils/ChangeSettings.java
@@ -4,8 +4,7 @@ import com.github.alfonsoleandro.autopickup.AutoPickup;
 import com.github.alfonsoleandro.autopickup.managers.AutoPickupSettings;
 import com.github.alfonsoleandro.mputils.managers.MessageSender;
 import org.bukkit.entity.Player;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Locale;
 
 public class ChangeSettings {
     private final AutoPickup plugin;
@@ -16,286 +15,181 @@ public class ChangeSettings {
         this.messageSender = plugin.getMessageSender();
     }
 
-    public boolean settingExists(String setting) {
-        String[] Settings = {"block-auto-pickup", "mob-auto-pickup", "exp-auto-pickup", "block-auto-smelt", "mob-auto-smelt", "careful-break", "careful-smelt"};
-        List<String> settingsList = Arrays.asList(Settings);
-        return settingsList.contains(setting);
+    /**
+     * Checks if a setting type exists or not in the list of available settings.
+     * @param setting The setting to try to change.
+     * @return true if the setting is invalid.
+     */
+    public boolean settingNotExists(String setting) {
+        return SettingType.getByString(setting) == null;
+    }
+
+    /**
+     * Checks if a player has the required permission for what they are about to do.
+     * @param settingType The type of setting they are going to change.
+     * @param player The player trying to change
+     * @param forOther TODO
+     * @return TODO
+     */
+    private boolean notHasPermission(SettingType settingType, Player player, boolean forOther){
+        if(forOther && !player.hasPermission("autoPickup."+settingType.getPermNode()+".others")){
+            this.messageSender.send(player, Message.NO_PERMISSION);
+            return true;
+        }
+        if(!forOther && !player.hasPermission("autoPickup."+settingType.getPermNode())){
+            this.messageSender.send(player, Message.NO_PERMISSION);
+            return true;
+        }
+        return false;
     }
 
     public void enableSetting(String setting, Player player, boolean forOther){
         AutoPickupSettings playerSettings = this.plugin.getAutoPickupManager().getPlayer(player.getName());
+        if(notHasPermission(SettingType.getByString(setting), player, forOther)) return;
+
         switch (setting) {
             case "block-auto-pickup" -> {
-                if(forOther && !player.hasPermission("autopickup.autopickup.blocks.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autopickup.blocks")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoPickupBlocks(true);
-                messageSender.send(player, Message.AP_BLOCKS_ENABLED);
+                this.messageSender.send(player, Message.AP_BLOCKS_ENABLED);
             }
             case "mob-auto-pickup" -> {
-                if(forOther && !player.hasPermission("autopickup.autopickup.mobs.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autopickup.mobs")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoPickupMobDrops(true);
-                messageSender.send(player, Message.AP_MOB_ENABLED);
+                this.messageSender.send(player, Message.AP_MOB_ENABLED);
             }
             case "exp-auto-pickup" -> {
-                if(forOther && !player.hasPermission("autopickup.autopickup.exp.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autopickup.exp")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoPickupExp(true);
-                messageSender.send(player, Message.AP_EXP_ENABLED);
+                this.messageSender.send(player, Message.AP_EXP_ENABLED);
             }
             case "block-auto-smelt" -> {
-                if(forOther && !player.hasPermission("autopickup.autosmelt.blocks.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autosmelt.blocks")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoSmeltBlocks(true);
-                messageSender.send(player, Message.AS_BLOCKS_ENABLED);
+                this.messageSender.send(player, Message.AS_BLOCKS_ENABLED);
             }
             case "mob-auto-smelt" -> {
-                if(forOther && !player.hasPermission("autopickup.autosmelt.mobs.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autosmelt.mobs")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoSmeltMobs(true);
-                messageSender.send(player, Message.AS_MOB_ENABLED);
+                this.messageSender.send(player, Message.AS_MOB_ENABLED);
             }
             case "careful-break" -> {
-                if(forOther && !player.hasPermission("autopickup.carefulbreak.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.carefulbreak")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setCarefulBreak(true);
-                messageSender.send(player, Message.CAREFUL_BREAK_ENABLED);
+                this.messageSender.send(player, Message.CAREFUL_BREAK_ENABLED);
             }
             case "careful-smelt" -> {
-                if(forOther && !player.hasPermission("autopickup.carefulsmelt.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.carefulsmelt")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setCarefulSmelt(true);
-                messageSender.send(player, Message.CAREFUL_SMELT_ENABLED);
+                this.messageSender.send(player, Message.CAREFUL_SMELT_ENABLED);
             }
         }
     }
 
     public void disableSetting(String setting, Player player, boolean forOther){
         AutoPickupSettings playerSettings = this.plugin.getAutoPickupManager().getPlayer(player.getName());
+        if(notHasPermission(SettingType.getByString(setting), player, forOther)) return;
+
         switch (setting) {
             case "block-auto-pickup" -> {
-                if(forOther && !player.hasPermission("autopickup.autopickup.blocks.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autopickup.blocks")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoPickupBlocks(false);
-                messageSender.send(player, Message.AP_BLOCKS_DISABLED);
+                this.messageSender.send(player, Message.AP_BLOCKS_DISABLED);
             }
             case "mob-auto-pickup" -> {
-                if(forOther && !player.hasPermission("autopickup.autopickup.mobs.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autopickup.mobs")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoPickupMobDrops(false);
-                messageSender.send(player, Message.AP_MOB_DISABLED);
+                this.messageSender.send(player, Message.AP_MOB_DISABLED);
             }
             case "exp-auto-pickup" -> {
-                if(forOther && !player.hasPermission("autopickup.autopickup.exp.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autopickup.exp")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoPickupExp(false);
-                messageSender.send(player, Message.AP_EXP_DISABLED);
+                this.messageSender.send(player, Message.AP_EXP_DISABLED);
             }
             case "block-auto-smelt" -> {
-                if(forOther && !player.hasPermission("autopickup.autosmelt.blocks.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autosmelt.blocks")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoSmeltBlocks(false);
-                messageSender.send(player, Message.AS_BLOCKS_DISABLED);
+                this.messageSender.send(player, Message.AS_BLOCKS_DISABLED);
             }
             case "mob-auto-smelt" -> {
-                if(forOther && !player.hasPermission("autopickup.autosmelt.mobs.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.autosmelt.mobs")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setAutoSmeltMobs(false);
-                messageSender.send(player, Message.AS_MOB_DISABLED);
+                this.messageSender.send(player, Message.AS_MOB_DISABLED);
             }
             case "careful-break" -> {
-                if(forOther && !player.hasPermission("autopickup.carefulbreak.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.carefulbreak")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setCarefulBreak(false);
-                messageSender.send(player, Message.CAREFUL_BREAK_DISABLED);
+                this.messageSender.send(player, Message.CAREFUL_BREAK_DISABLED);
             }
             case "careful-smelt" -> {
-                if(forOther && !player.hasPermission("autopickup.carefulsmelt.others")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if(!player.hasPermission("autopickup.carefulsmelt")){
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 playerSettings.setCarefulSmelt(false);
-                messageSender.send(player, Message.CAREFUL_SMELT_DISABLED);
+                this.messageSender.send(player, Message.CAREFUL_SMELT_DISABLED);
             }
         }
     }
 
     public void toggleSetting(String setting, Player player, boolean forOther) {
         AutoPickupSettings playerSettings = this.plugin.getAutoPickupManager().getPlayer(player.getName());
+        if(notHasPermission(SettingType.getByString(setting), player, forOther)) return;
+
         switch (setting) {
             case "block-auto-pickup" -> {
-                if (forOther && !player.hasPermission("autopickup.autopickup.blocks.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.autopickup.blocks")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.autoPickupBlocksEnabled();
                 playerSettings.setAutoPickupBlocks(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.AP_BLOCKS_DISABLED : Message.AP_BLOCKS_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.AP_BLOCKS_DISABLED : Message.AP_BLOCKS_ENABLED);
             }
             case "mob-auto-pickup" -> {
-                if (forOther && !player.hasPermission("autopickup.autopickup.mobs.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.autopickup.mobs")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.autoPickupMobDropsEnabled();
                 playerSettings.setAutoPickupMobDrops(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.AP_MOB_DISABLED : Message.AP_MOB_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.AP_MOB_DISABLED : Message.AP_MOB_ENABLED);
             }
             case "exp-auto-pickup" -> {
-                if (forOther && !player.hasPermission("autopickup.autopickup.exp.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.autopickup.exp")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.autoPickupExpEnabled();
                 playerSettings.setAutoPickupExp(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.AP_EXP_DISABLED : Message.AP_EXP_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.AP_EXP_DISABLED : Message.AP_EXP_ENABLED);
             }
             case "block-auto-smelt" -> {
-                if (forOther && !player.hasPermission("autopickup.autosmelt.blocks.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.autosmelt.blocks")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.autoSmeltBlocksEnabled();
                 playerSettings.setAutoSmeltBlocks(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.AS_BLOCKS_DISABLED : Message.AS_BLOCKS_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.AS_BLOCKS_DISABLED : Message.AS_BLOCKS_ENABLED);
             }
             case "mob-auto-smelt" -> {
-                if (forOther && !player.hasPermission("autopickup.autosmelt.mobs.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.autosmelt.mobs")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.autoSmeltMobEnabled();
                 playerSettings.setAutoSmeltMobs(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.AS_MOB_DISABLED : Message.AS_MOB_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.AS_MOB_DISABLED : Message.AS_MOB_ENABLED);
             }
             case "careful-break" -> {
-                if (forOther && !player.hasPermission("autopickup.carefulbreak.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.carefulbreak")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.carefulBreakEnabled();
                 playerSettings.setCarefulBreak(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.CAREFUL_BREAK_DISABLED : Message.CAREFUL_BREAK_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.CAREFUL_BREAK_DISABLED : Message.CAREFUL_BREAK_ENABLED);
             }
             case "careful-smelt" -> {
-                if (forOther && !player.hasPermission("autopickup.carefulsmelt.others")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
-                if (!player.hasPermission("autopickup.carefulsmelt")) {
-                    messageSender.send(player, Message.NO_PERMISSION);
-                    break;
-                }
                 boolean wasEnabled = playerSettings.carefulSmeltEnabled();
                 playerSettings.setCarefulSmelt(!wasEnabled);
-                messageSender.send(player, wasEnabled ? Message.CAREFUL_SMELT_DISABLED : Message.CAREFUL_SMELT_ENABLED);
+                this.messageSender.send(player, wasEnabled ? Message.CAREFUL_SMELT_DISABLED : Message.CAREFUL_SMELT_ENABLED);
             }
+        }
+    }
+
+
+    enum SettingType{
+        BLOCK_AUTO_PICKUP("autoPickup.blocks"),
+        MOB_AUTO_PICKUP("autoPickup.mobs"),
+        EXP_AUTO_PICKUP("autoPickup.exp"),
+        BLOCK_AUTO_SMELT("autoSmelt.blocks"),
+        MOB_AUTO_SMELT("autoSmelt.mobs"),
+        CAREFUL_BREAK("carefulBreak"),
+        CAREFUL_SMELT("carefulSmelt");
+        
+        
+        private final String permNode;
+        
+        SettingType(String permNode){
+            this.permNode = permNode;    
+        }
+
+        public String getPermNode() {
+            return this.permNode;
+        }
+
+        public static SettingType getByString(String settingType){
+            return switch (settingType.toLowerCase(Locale.ROOT)) {
+                case "block-auto-pickup" -> BLOCK_AUTO_PICKUP;
+                case "mob-auto-pickup" -> MOB_AUTO_PICKUP;
+                case "exp-auto-pickup" -> EXP_AUTO_PICKUP;
+                case "block-auto-smelt" -> BLOCK_AUTO_SMELT;
+                case "mob-auto-smelt" -> MOB_AUTO_SMELT;
+                case "careful-break" -> CAREFUL_BREAK;
+                case "careful-smelt" -> CAREFUL_SMELT;
+                default -> null;
+            };
         }
     }
 }
